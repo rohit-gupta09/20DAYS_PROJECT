5e3f3cdd3d082f08e955ad75df7196a2bc8e91b9
Rohit Gupta <gupta2002rohit@gmail.com>
2021-08-26 16:15:18 +0530

    Project

---
+++ Light & Dark Mode/script.js
@@ -0,0 +1,64 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav=document.getElementById('nav');
+const toggleIcon=document.getElementById('toggle-icon');
+const image1=document.getElementById('image1');
+const image2=document.getElementById('image2');
+const image3=document.getElementById('image3');
+const textbox=document.getElementById('text-box');
+
+// dark mode style
+
+function darkmode(){
+    nav.style.backgroundColor='rgb(0 0 0 / 50%)';
+    textbox.style.backgroundColor='rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent='Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    image1.src='img/undraw_proud_coder_dark.svg';
+    image2.src='img/undraw_feeling_proud_dark.svg';
+    image3.src='img/undraw_conceptual_idea_dark.svg';
+}
+
+// Ligth mode style
+
+function lightmode(){
+    nav.style.backgroundColor='rgb(255 255 255 / 50%)';
+    textbox.style.backgroundColor='rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent='Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    image1.src='img/undraw_proud_coder_light.svg';
+    image2.src='img/undraw_feeling_proud_light.svg';
+    image3.src='img/undraw_conceptual_idea_light.svg';
+}
+
+//switch thwmw dynamcally
+function switchTheme(event){
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+        localStorage.setItem('theme','dark');
+        darkmode();
+    }
+    else{
+        document.documentElement.setAttribute('data-theme','light');
+        localStorage.setItem('theme','light');
+        lightmode();
+    }
+}
+
+// Event Listener
+
+toggleSwitch.addEventListener('change',switchTheme);
+
+// Check loacal for theme
+
+const currentTheme=localStorage.getItem('theme');
+
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme',currentTheme);
+    
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked=true;
+        darkmode();
+    }
+}
